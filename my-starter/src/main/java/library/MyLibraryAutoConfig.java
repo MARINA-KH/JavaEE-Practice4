26c8e83ae1727ff583462b422db0e53f86539b1f
Marina Kharchenko <jkh93955@gmail.com>
2020-02-29 22:15:32 +0200

    Final

---
+++ my-starter/src/main/java/library/MyLibraryAutoConfig.java
@@ -0,0 +1,22 @@
+package library;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyLibraryAutoConfig {
+
+    @Bean
+    @ConditionalOnProperty(value = "my.property", havingValue = "default")
+    MyLibraryClass myStarterClassDefault() {
+        return new MyLibraryClass("default");
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "my.property", havingValue = "custom")
+    MyLibraryClass myStarterClassCustom() {
+        return new MyLibraryClass("custom");
+    }
+
+}
---
+++ my-starter/src/main/java/library/MyLibraryClass.java
@@ -0,0 +1,15 @@
+package library;
+
+public class MyLibraryClass {
+
+    private String info;
+
+    public void printInfo() {
+        System.out.println(info);
+    }
+
+    MyLibraryClass(String info) {
+        this.info = info;
+    }
+
+}
